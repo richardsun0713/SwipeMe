8c41ce324ff6928ed1446a97446747496f08eb98
Jason Zheng <jwzheng94@gmail.com>
2015-02-24 21:22:41 -0800

    Fixed Merge and added Joy's layout changes. Changed fragment_home layout so buttons are symmetric and have borders Fixed AndroidManifest so logout only needs to be pressed once

--- app/src/main/java/com/swipeme/www/swipeme/HomeActivity.java
+++ app/src/main/java/com/swipeme/www/swipeme/HomeActivity.java
@@ -1,11 +1,9 @@
 package com.swipeme.www.swipeme;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,6 +15,7 @@ import com.facebook.Session;
 
 public class HomeActivity extends ActionBarActivity {
 
+    private static final String TAG = "HomeActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,7 +79,6 @@ public class HomeActivity extends ActionBarActivity {
             //clear your preferences if saved
 
         }
-
         // Finish current activity
         finish();
     }
